a8fdac13321b5b9a0b2c59e0698e1ee489f9629a
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2023-07-14 22:08:22 -0300

    test: ensure SignUpController return an error if no email provided

--- test/presentation/controllers/singup.spec.ts
+++ test/presentation/controllers/singup.spec.ts
@@ -15,4 +15,18 @@ describe('SingUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: name'));
   });
+
+  test('Sould return 400 if no email provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
+  });
 });
