8cde11ec1bd442819fb44da8a640db1f1233007e
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2023-07-16 20:05:01 -0300

    test: ensure SignUpController return 400 if no password confirmation is provided

--- test/presentation/controllers/singup.spec.ts
+++ test/presentation/controllers/singup.spec.ts
@@ -31,7 +31,7 @@ describe('SingUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('email'));
   });
 
-  test('Sould return 400 if no password provided', () => {
+  test('Sould return 400 if no password is provided', () => {
     const sut = new SignUpController();
     const httpRequest = {
       body: {
@@ -42,5 +42,19 @@ describe('SingUp Controller', () => {
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('password'));
+  });
+
+  test('Sould return 400 if no password confirmation is provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'));
   });
 });
