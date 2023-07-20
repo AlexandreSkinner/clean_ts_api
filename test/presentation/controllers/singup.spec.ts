903a026748ea89a31814f8cf55f5719890ad3446
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2023-07-19 21:06:45 -0300

    test: ensure SignUpController calls EmailValidator with correct email

--- test/presentation/controllers/singup.spec.ts
+++ test/presentation/controllers/singup.spec.ts
@@ -93,4 +93,19 @@ describe('SingUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new InvalidParamError('email'));
   });
+
+  test('Sould call EmailValidator with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = vitest.spyOn(emailValidatorStub, 'isValid');
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    };
+    sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com');
+  });
 });
