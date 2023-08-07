ff86e16542ad35d621235704eecdf58b4972c50e
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2023-08-07 10:45:39 -0300

    refactor: remove any return type from handle

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -11,7 +11,7 @@ export class SignUpController implements Controller {
     this.addAccount = addAccount;
   }
 
-  handle (httpRequest: HttpRequest): HttpResponse | any {
+  handle (httpRequest: HttpRequest): HttpResponse {
     try {
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
       for (const field of requiredFields) {
