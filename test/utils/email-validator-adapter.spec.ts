6b30dc1f4a14d398cd936e36480f6b22917a9ebb
Alexandre Skinner <AlexandreSkinner@tmassessoria.com.br>
2023-08-19 21:49:36 -0300

    refactor: move sut creation to a factory helper method

--- test/utils/email-validator-adapter.spec.ts
+++ test/utils/email-validator-adapter.spec.ts
@@ -8,22 +8,26 @@ vitest.doMock('validator', () => ({
   }
 }));
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter();
+};
+
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator return false', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     vitest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
     const isValid = sut.isValid('invalid_email@mail.com');
     expect(isValid).toBe(false);
   });
 
   test('Should return true if validator return true', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isValid = sut.isValid('valid_email@mail.com');
     expect(isValid).toBe(true);
   });
 
   test('Should call validator with correct email', () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isEmailSpy = vitest.spyOn(validator, 'isEmail');
     sut.isValid('any_email@mail.com');
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com');
